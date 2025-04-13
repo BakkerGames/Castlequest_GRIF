ec30cbda33b14641d984b8bbd5286ffcc256353c
Scott Bakker <sbakker343@gmail.com>
2025-04-13 15:27:45 -0400

    test batch files

---
+++ Testing/Castlequest Maps.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\Scott\EXE\grif.exe" Castlequest.grif -m Mods\corrections.grif -m Mods\roommaps.grif -m Mods\notes.grif
+pause
---
+++ Testing/Castlequest.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\Scott\EXE\grif.exe" Castlequest.grif
+pause
